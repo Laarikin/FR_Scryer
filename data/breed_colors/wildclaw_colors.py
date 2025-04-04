8ab2f6d1feb7c2444a70dc36173369ee240680bc
Laarikin <laarikin@gmail.com>
2025-04-04 11:59:17 -0400

    Finished Adding all Colors

--- data/breed_colors/wildclaw_colors.py
+++ data/breed_colors/wildclaw_colors.py
@@ -437,3 +437,627 @@ WILDCLAW_COLOR_MAP = {
         "horns": "1b6b98"
         "feathers": "84eede"
     },
+    "Jade": {
+        "base": "61ab89"
+        "belly": "337d5b"
+        "horns": "434343"
+        "feathers": "4b8e6f"
+    },
+    "Spearmint": {
+        "base": "158e67"
+        "belly": "25ebac"
+        "horns": "51fff1"
+        "feathers": "1bb584"
+    },
+    "Thicket": {
+        "base": "005f49"
+        "belly": "158e67"
+        "horns": "093d41"
+        "feathers": "004735"
+    },
+    "Peacock": {
+        "base": "1f4739"
+        "belly": "0f1f29"
+        "horns": "63b777"
+        "feathers": "2b6751"
+    },
+    "Emerald": {
+        "base": "21613f"
+        "belly": "6f7b3f"
+        "horns": "c1d95b"
+        "feathers": "1f5b3d"
+    },
+    "Shamrock": {
+        "base": "236925"
+        "belly": "004309"
+        "horns": "1b331d"
+        "feathers": "1b4f1d"
+    },
+    "Jungle": {
+        "base": "1f371b"
+        "belly": "3f6131"
+        "horns": "598c35"
+        "feathers": "192d17"
+    },
+    "Hunter": {
+        "base": "1d2715"
+        "belly": "2b3d1f"
+        "horns": "2f2919"
+        "feathers": "293d1b"
+    },
+    "Forest": {
+        "base": "435135"
+        "belly": "27331d"
+        "horns": "879b75"
+        "feathers": "5d7149"
+    },
+    "Camo": {
+        "base": "51694d"
+        "belly": "9db780"
+        "horns": "9db780"
+        "feathers": "717d51"
+    },
+    "Algae": {
+        "base": "97af8b"
+        "belly": "556555"
+        "horns": "2b3729"
+        "feathers": "73815f"
+    },
+    "Swamp": {
+        "base": "697f67"
+        "belly": "a2a575"
+        "horns": "978865"
+        "feathers": "919075"
+    },
+    "Avocado": {
+        "base": "577d35"
+        "belly": "9ec35d"
+        "horns": "b0ff25"
+        "feathers": "6d9947"
+    },
+    "Green": {
+        "base": "639c3f"
+        "belly": "67b47d"
+        "horns": "47455d"
+        "feathers": "7fc057"
+    },
+    "Fern": {
+        "base": "7fce73"
+        "belly": "c7f690"
+        "horns": "1f5925"
+        "feathers": "539e49"
+    },
+    "Mantis": {
+        "base": "99ff9c"
+        "belly": "65b85b"
+        "horns": "e9ffbe"
+        "feathers": "87ff71"
+    },
+    "Pear": {
+        "base": "8ecd55"
+        "belly": "b7ff73"
+        "horns": "dee973"
+        "feathers": "69ab45"
+    },
+    "Leaf": {
+        "base": "a5e32d"
+        "belly": "65920d"
+        "horns": "21532d"
+        "feathers": "73c323"
+    },
+    "Radioactive": {
+        "base": "c6ff00"
+        "belly": "67ff00"
+        "horns": "00ffcc"
+        "feathers": "88c000"
+    },
+    "Honeydew": {
+        "base": "d0e673"
+        "belly": "fbffad"
+        "horns": "fffae4"
+        "feathers": "d8e88a"
+    },
+    "Peridot": {
+        "base": "e8ffb5"
+        "belly": "b4d473"
+        "horns": "6f8737"
+        "feathers": "afcf69"
+    },
+    "Chartreuse": {
+        "base": "b4cd3d"
+        "belly": "f6ffb4"
+        "horns": "b18211"
+        "feathers": "e8ff77"
+    },
+    "Spring": {
+        "base": "a9a033"
+        "belly": "eea447"
+        "horns": "d9a97f"
+        "feathers": "8ea933"
+    },
+    "Crocodile": {
+        "base": "828335"
+        "belly": "5b5325"
+        "horns": "2d3907"
+        "feathers": "616f2b"
+    },
+    "Olive": {
+        "base": "697135"
+        "belly": "bcbb5b"
+        "horns": "dff069"
+        "feathers": "a8a63d"
+    },
+    "Murk": {
+        "base": "4b4521"
+        "belly": "2f2911"
+        "horns": "836d37"
+        "feathers": "5d532d"
+    },
+    "Moss": {
+        "base": "7f7745"
+        "belly": "414327"
+        "horns": "3f2d11"
+        "feathers": "57512b"
+    },
+    "Goldenrod": {
+        "base": "bea55d"
+        "belly": "ba8d27"
+        "horns": "aa8100"
+        "feathers": "b59c57"
+    },
+    "Amber": {
+        "base": "c18e1b"
+        "belly": "ffdf5f"
+        "horns": "ffdc8a"
+        "feathers": "cf9b25"
+    },
+    "Honey": {
+        "base": "d1b300"
+        "belly": "fff54d"
+        "horns": "7d5500"
+        "feathers": "fff83b"
+    },
+    "Lemon": {
+        "base": "ffe63b"
+        "belly": "9f7139"
+        "horns": "534500"
+        "feathers": "c3b135"
+    },
+    "Yellow": {
+        "base": "f9e255"
+        "belly": "f6cc3f"
+        "horns": "be9a39"
+        "feathers": "f6cc3f"
+    },
+    "Grapefruit": {
+        "base": "f7ff6f"
+        "belly": "e9815d"
+        "horns": "ffffff"
+        "feathers": "ffd9a4"
+    },
+    "Banana": {
+        "base": "ffec80"
+        "belly": "fff5bd"
+        "horns": "ffeeb1"
+        "feathers": "fff19c"
+    },
+    "Sanddollar": {
+        "base": "ebe7ae"
+        "belly": "d8b75b"
+        "horns": "fffee6"
+        "feathers": "c7c280"
+    },
+    "Flaxen": {
+        "base": "fde9ae"
+        "belly": "d19127"
+        "horns": "a56100"
+        "feathers": "fadf86"
+    },
+    "Ivory": {
+        "base": "ffd297"
+        "belly": "ffdbb1"
+        "horns": "fffaf3"
+        "feathers": "ffe9b4"
+    },
+    "Buttercup": {
+        "base": "f6bf6b"
+        "belly": "ffb84d"
+        "horns": "d48200"
+        "feathers": "ffd492"
+    },
+    "Gold": {
+        "base": "e8af49"
+        "belly": "f4bc7f"
+        "horns": "f5caa2"
+        "feathers": "f6cd5d"
+    },
+    "Metals": {
+        "base": "d1b047"
+        "belly": "c4c6ce"
+        "horns": "853f07"
+        "feathers": "b8cada"
+    },
+    "Marigold": {
+        "base": "ffb43b"
+        "belly": "fa912b"
+        "horns": "fff6dc"
+        "feathers": "eca027"
+    },
+    "Sunshine": {
+        "base": "fa912b"
+        "belly": "a45925"
+        "horns": "5b372b"
+        "feathers": "e38c37"
+    },
+    "Saffron": {
+        "base": "ff8400"
+        "belly": "ffad2b"
+        "horns": "ffc22d"
+        "feathers": "d96500"
+    },
+    "Sunset": {
+        "base": "ffa249"
+        "belly": "e77709"
+        "horns": "8d4500"
+        "feathers": "ff9a00"
+    },
+    "Peach": {
+        "base": "ffb577"
+        "belly": "ffd38c"
+        "horns": "ffedac"
+        "feathers": "ffd394"
+    },
+    "Cantaloupe": {
+        "base": "ff984f"
+        "belly": "ffd0a9"
+        "horns": "ffffff"
+        "feathers": "ffbb8e"
+    },
+    "Orange": {
+        "base": "d5612b"
+        "belly": "ee8847"
+        "horns": "daab7f"
+        "feathers": "eb9d4b"
+    },
+    "Bronze": {
+        "base": "b2570d"
+        "belly": "693509"
+        "horns": "572700"
+        "feathers": "753300"
+    },
+    "Terracotta": {
+        "base": "b23b07"
+        "belly": "e1854d"
+        "horns": "8e2507"
+        "feathers": "c34b15"
+    },
+    "Carrot": {
+        "base": "ff5500"
+        "belly": "802711"
+        "horns": "ffb596"
+        "feathers": "ab3900"
+    },
+    "Fire": {
+        "base": "ef5d23"
+        "belly": "a54135"
+        "horns": "5b332f"
+        "feathers": "d04915"
+    },
+    "Pumpkin": {
+        "base": "ff6941"
+        "belly": "ffa269"
+        "horns": "ffc289"
+        "feathers": "ff904f"
+    },
+    "Tangerine": {
+        "base": "ff7361"
+        "belly": "c03923"
+        "horns": "ba4737"
+        "feathers": "e05745"
+    },
+    "Cinnamon": {
+        "base": "c05b39"
+        "belly": "ffa873"
+        "horns": "672713"
+        "feathers": "a14b2f"
+    },
+    "Caramel": {
+        "base": "c67147"
+        "belly": "7b3b2b"
+        "horns": "ffb483"
+        "feathers": "e3935d"
+    },
+    "Sand": {
+        "base": "b27749"
+        "belly": "d9aa77"
+        "horns": "dc8a65"
+        "feathers": "9a6339"
+    },
+    "Tan": {
+        "base": "c49a71"
+        "belly": "e4c4a6"
+        "horns": "f7e2c6"
+        "feathers": "cbaa8b"
+    },
+    "Beige": {
+        "base": "cabba2"
+        "belly": "736d57"
+        "horns": "433913"
+        "feathers": "ad9a80"
+    },
+    "Stone": {
+        "base": "827b65"
+        "belly": "e0ddc0"
+        "horns": "cdc5ac"
+        "feathers": "9f9477"
+    },
+    "Taupe": {
+        "base": "6d675b"
+        "belly": "453f39"
+        "horns": "2d2b2b"
+        "feathers": "595147"
+    },
+    "Slate": {
+        "base": "574d49"
+        "belly": "8a7f79"
+        "horns": "c3bbb2"
+        "feathers": "6d5f55"
+    },
+    "Driftwood": {
+        "base": "776359"
+        "belly": "92837d"
+        "horns": "b2a8a2"
+        "feathers": "5f514b"
+    },
+    "Latte": {
+        "base": "977b6d"
+        "belly": "cbae9e"
+        "horns": "fff2d8"
+        "feathers": "69554b"
+    },
+    "Dirt": {
+        "base": "77493f"
+        "belly": "9a654b"
+        "horns": "b8856b"
+        "feathers": "8d675b"
+    },
+    "Clay": {
+        "base": "613f3d"
+        "belly": "a66f67"
+        "horns": "351f29"
+        "feathers": "754947"
+    },
+    "Sable": {
+        "base": "57372d"
+        "belly": "31211b"
+        "horns": "9e6d55"
+        "feathers": "371b15"
+    },
+    "Umber": {
+        "base": "2f1f1b"
+        "belly": "613527"
+        "horns": "8f532b"
+        "feathers": "432b1f"
+    },
+    "Soil": {
+        "base": "5b4535"
+        "belly": "88775d"
+        "horns": "311b09"
+        "feathers": "45392f"
+    },
+    "Hickory": {
+        "base": "735739"
+        "belly": "a88e65"
+        "horns": "cfc197"
+        "feathers": "98794f"
+    },
+    "Tarnish": {
+        "base": "855d33"
+        "belly": "b0a46f"
+        "horns": "4b7779"
+        "feathers": "a86f37"
+    },
+    "Ginger": {
+        "base": "90532b"
+        "belly": "653705"
+        "horns": "d7c4e6"
+        "feathers": "ba651b"
+    },
+    "Brown": {
+        "base": "8e5b3f"
+        "belly": "55332b"
+        "horns": "372d23"
+        "feathers": "714b35"
+    },
+    "Chocolate": {
+        "base": "573113"
+        "belly": "835539"
+        "horns": "351903"
+        "feathers": "6d411d"
+    },
+    "Auburn": {
+        "base": "7b3d1d"
+        "belly": "512713"
+        "horns": "c0733d"
+        "feathers": "552715"
+    },
+    "Copper": {
+        "base": "a44b29"
+        "belly": "a28e7d"
+        "horns": "7b250b"
+        "feathers": "cb673f"
+    },
+    "Rust": {
+        "base": "8b3321"
+        "belly": "c84d31"
+        "horns": "db8259"
+        "feathers": "752919"
+    },
+    "Tomato": {
+        "base": "ba311d"
+        "belly": "e64731"
+        "horns": "fe4f1f"
+        "feathers": "9d2715"
+    },
+    "Vermilion": {
+        "base": "e22d17"
+        "belly": "ff614d"
+        "horns": "6b0b15"
+        "feathers": "bf1101"
+    },
+    "Ruby": {
+        "base": "cd000f"
+        "belly": "770009"
+        "horns": "5f0100"
+        "feathers": "f80011"
+    },
+    "Cherry": {
+        "base": "aa0025"
+        "belly": "ff192f"
+        "horns": "45171d"
+        "feathers": "e7172f"
+    },
+    "Crimson": {
+        "base": "850013"
+        "belly": "510f15"
+        "horns": "250f11"
+        "feathers": "770d1b"
+    },
+    "Garnet": {
+        "base": "5b0f15"
+        "belly": "9f0d29"
+        "horns": "c1212d"
+        "feathers": "3b070b"
+    },
+    "Sanguine": {
+        "base": "2f0003"
+        "belly": "590917"
+        "horns": "a01b21"
+        "feathers": "63000b"
+    },
+    "Blood": {
+        "base": "451717"
+        "belly": "290f0f"
+        "horns": "171311"
+        "feathers": "5b2b2b"
+    },
+    "Maroon": {
+        "base": "652127"
+        "belly": "93412f"
+        "horns": "3f292b"
+        "feathers": "863339"
+    },
+    "Berry": {
+        "base": "8b272d"
+        "belly": "bc5549"
+        "horns": "ffd3aa"
+        "feathers": "79171d"
+    },
+    "Red": {
+        "base": "c1272d"
+        "belly": "ff613d"
+        "horns": "daab7f"
+        "feathers": "e62d35"
+    },
+    "Strawberry": {
+        "base": "de3335"
+        "belly": "a5252f"
+        "horns": "ff7579"
+        "feathers": "ff4f53"
+    },
+    "Cerise": {
+        "base": "a22929"
+        "belly": "d88fa7"
+        "horns": "d5a4dc"
+        "feathers": "9a3349"
+    },
+    "Carmine": {
+        "base": "b13b3b"
+        "belly": "6f293d"
+        "horns": "2d1b21"
+        "feathers": "963535"
+    },
+    "Brick": {
+        "base": "9a534d"
+        "belly": "672929"
+        "horns": "e1b4a0"
+        "feathers": "84433f"
+    },
+    "Coral": {
+        "base": "cc6f6f"
+        "belly": "f6c4a5"
+        "horns": "f8aea9"
+        "feathers": "e78585"
+    },
+    "Blush": {
+        "base": "ffa2a2"
+        "belly": "c64965"
+        "horns": "f8dfe1"
+        "feathers": "e58580"
+    },
+    "Cottoncandy": {
+        "base": "eb7997"
+        "belly": "ffaaae"
+        "horns": "ad3351"
+        "feathers": "ff959e"
+    },
+    "Watermelon": {
+        "base": "db518d"
+        "belly": "a22b43"
+        "horns": "ff96ac"
+        "feathers": "a4275d"
+    },
+    "Magenta": {
+        "base": "e935aa"
+        "belly": "ff81ad"
+        "horns": "5b030d"
+        "feathers": "ff5bc6"
+    },
+    "Fuchsia": {
+        "base": "ec0089"
+        "belly": "810d49"
+        "horns": "f759cf"
+        "feathers": "9a095d"
+    },
+    "Raspberry": {
+        "base": "8a0349"
+        "belly": "da518c"
+        "horns": "ffffff"
+        "feathers": "5b0031"
+    },
+    "Wine": {
+        "base": "4d0f29"
+        "belly": "a10559"
+        "horns": "cc77a7"
+        "feathers": "71213f"
+    },
+    "Mauve": {
+        "base": "9c4975"
+        "belly": "f86dcc"
+        "horns": "fa63a8"
+        "feathers": "792d4f"
+    },
+    "Pink": {
+        "base": "e77fbf"
+        "belly": "d8c6e4"
+        "horns": "d3afc4"
+        "feathers": "ff9ce5"
+    },
+    "Bubblegum": {
+        "base": "eaa9ff"
+        "belly": "ffd8f9"
+        "horns": "ca59ff"
+        "feathers": "da95f0"
+    },
+    "Rose": {
+        "base": "ffd6f6"
+        "belly": "f79fc9"
+        "horns": "ffffff"
+        "feathers": "ffb6ef"
+    },
+    "Pearl": {
+        "base": "fbedf9"
+        "belly": "ffffff"
+        "horns": "d7b0ff"
+        "feathers": "edd1fa"
+    },
